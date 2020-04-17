55f046522d4a942db14d373c7456f2dd83981ff9
hseka <sekaherve@gmail.com>
2020-04-17 11:59:35 +0200

    Use Templating Service in the Controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,6 +9,7 @@ use DateTime;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -40,5 +41,20 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+  /**
+   * @param $geniusName
+   * @Route("/genus/{geniusName}")
+   * @return Response
+   */
+    public function showAction($geniusName)
+    {
+        $templating = $this->container->get('templating');
+        $html = $templating->render('genus/show.html.twig', array(
+          'name' => $geniusName
+        ));
+
+        return new Response($html);
     }
 }
