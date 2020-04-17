5fffe23d25f41f18c00cbcea45e78269f21e9266
hseka <sekaherve@gmail.com>
2020-04-17 19:52:05 +0200

    Finish the course

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,16 +6,20 @@ use AppBundle\Entity\Exp;
 use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
 use DateTime;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
+  /**
+   * @Route("/", name="homepage")
+   * @param Request $request
+   * @return Response
+   */
     public function indexAction(Request $request)
     {
         $user = new User();
@@ -50,11 +54,26 @@ class DefaultController extends Controller
    */
     public function showAction($geniusName)
     {
-        $templating = $this->container->get('templating');
-        $html = $templating->render('genus/show.html.twig', array(
+        return $this->render('genus/show.html.twig', array(
           'name' => $geniusName
         ));
+    }
 
-        return new Response($html);
+  /**
+   * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+   * @Method("GET")
+   */
+    public function getNotesAction()
+    {
+      $notes = [
+        ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+        ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+        ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+      ];
+      $data = [
+        'notes' => $notes
+      ];
+
+      return new JsonResponse($data);
     }
 }
