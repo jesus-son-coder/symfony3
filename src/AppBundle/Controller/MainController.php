4c9e6d885eb9165d23921bf5a9b59b99b0639502
hseka <sekaherve@gmail.com>
2020-04-18 14:50:08 +0200

    Configurations manipulations

---
+++ src/AppBundle/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController extends Controller
+{
+    public function homepageAction(Request $request)
+    {
+        return new Response("Hi! I'm there");
+    }
+
+}
