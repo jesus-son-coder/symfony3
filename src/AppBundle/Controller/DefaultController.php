1c82b80dcef5c74f5b9ecf467cf4af97c6e35a2c
hseka <sekaherve@gmail.com>
2020-04-23 19:16:55 +0200

    admin function

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,4 +23,19 @@ class DefaultController extends Controller
     }
 
 
+  /**
+   * @Route("/admin", name="admin_homepage")
+   * @param Request $request
+   *
+   * @Security("is_granted('ROLE_ADMIN')")
+   *
+   * @return Response
+   */
+  public function adminIndexAction(Request $request)
+  { /*
+    if(!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+      throw $this->createAccessDeniedException('GET OUT !');
+    } */
+    return $this->render('genus/admin-index.html.twig');
+  }
 }
