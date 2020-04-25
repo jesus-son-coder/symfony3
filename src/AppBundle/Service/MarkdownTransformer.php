de971917fb33805d946221a022896045ff6b14d0
hseka <sekaherve@gmail.com>
2020-04-26 00:35:48 +0200

    Inject (dependency injection) the Cache Service

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -9,24 +9,39 @@
 namespace AppBundle\Service;
 
 
+use Doctrine\Common\Cache\Cache;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 
 class MarkdownTransformer
 {
   private $markdownParser;
+  private $cache;
 
   /**
    * MarkdownTransformer constructor.
+   * @param MarkdownParserInterface $markdownParser
+   * @param Cache $cache
    */
-  public function __construct(MarkdownParserInterface $markdownParser)
+  public function __construct(MarkdownParserInterface $markdownParser, Cache $cache)
   {
     $this->markdownParser = $markdownParser;
+    $this->cache = $cache;
   }
 
   public function parse($str)
   {
-    return $this->markdownParser
+    $cache = $this->cache;
+    $key = md5($str);
+    if ($cache->contains($key)) {
+      $funFact = $cache->fetch($key);
+    }
+
+    sleep(1);
+    $str = $this->markdownParser
       ->transformMarkdown($str);
+    $cache->save($key, $str);
+
+    return $str;
   }
 
 
